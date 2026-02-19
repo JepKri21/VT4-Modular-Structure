08b5ef168ff17f77d287015bb77a3379271a00b4
LucasBoonde <lucas.nesgaard.bonde@gmail.com>
2026-02-19 09:38:15 +0100

    Nothing changed

--- made-react-app/app/page.tsx
+++ made-react-app/app/page.tsx
@@ -8,6 +8,7 @@ const Home = () => {
     <main>
       <div className="flex h-screen w-full items-center justify-center bg-primary">
         <h1 className="text-3xl font-bold text-primary">Welcome! 👋🏻</h1>
+        
         <Hello />
       </div>
     </main>
